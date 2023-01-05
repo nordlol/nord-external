d03eff82b6de2fa65cc48e165aed407d13f019fd
Buffer Overflow <randomusernameemail2021@gmail.com>
2023-01-05 17:29:52 -0500

    Add sigs for health and print.

--- nord/src/data/roblox/offsets.cpp
+++ nord/src/data/roblox/offsets.cpp
@@ -6,9 +6,16 @@ namespace nord::rbx
 {
     bool offsets::initialize()
     {
-        print = process_hook_mgr.mem.proc->format( 0x10358F0 );
+        if ( !init_print() )
+            return false;
 
         if ( !init_get_task_scheduler() )
+            return false;
+
+        if ( !init_get_health() )
+            return false;
+
+        if ( !init_get_max_health() )
             return false;
 
         log_mgr.log_debug( "offsets", "Successfully initialized offsets\n" );
@@ -20,7 +27,7 @@ namespace nord::rbx
         if ( !( get_task_scheduler = process_hook_mgr.mem.proc->sig_scan(
                     "\x55\x8B\xEC\x83\xE4\xF8\x83\xEC\x08\xE8\x00\x00\x00\x00\x8D\x0C\x24", "xxxxxxxxxx????xxx" ) ) )
         {
-            log_mgr.log_error( "offsets", "Failed to locate RBX::TaskScheduler signature\n" );
+            log_mgr.log_error( "offsets", "Failed to locate RBX::TaskScheduler::singleton() signature\n" );
             return false;
         }
 
@@ -29,6 +36,46 @@ namespace nord::rbx
             ( get_task_scheduler + 14 ) + process_hook_mgr.mem.proc->read< std::uintptr_t >( get_task_scheduler + 10 );
 
         log_mgr.log_debug( "offsets", "Located RBX::TaskScheduler::singleton() at 0x%x\n", get_task_scheduler );
+        return true;
+    }
+
+    bool offsets::init_print()
+    {
+        if ( !( print = process_hook_mgr.mem.proc->sig_scan(
+                    "\x55\x8B\xEC\x6A\xFF\x68\xCC\xCC\xCC\xCC\x64\xA1\x00\x00\x00\x00\x50\x64\x89\x25\x00\x00\x00\x00\x83"
+                    "\xEC\x1C\x8B\x55\x0C\x8D",
+                    "xxxxxx????xxxxxxxxxxxxxxxxxxxxx" ) ) )
+        {
+            log_mgr.log_error( "offsets", "Failed to locate RBX::StandardOut::print() signature\n" );
+            return false;
+        }
+
+        log_mgr.log_debug( "offsets", "Located RBX::StandardOut::print() at 0x%x\n", print );
+        return true;
+    }
+
+    bool offsets::init_get_health()
+    {
+        if ( !( get_health = process_hook_mgr.mem.proc->sig_scan( "\x55\x8B\xEC\x51\x8B\x81\x14", "xxxxxxx" ) ) )
+        {
+            log_mgr.log_error( "offsets", "Failed to locate RBX::Humanoid::getHealth() signature\n" );
+            return false;
+        }
+
+        log_mgr.log_debug( "offsets", "Located RBX::Humanoid::getHealth() at 0x%x\n", get_health );
+        return true;
+    }
+
+    bool offsets::init_get_max_health()
+    {
+        if ( !( get_max_health =
+                    process_hook_mgr.mem.proc->sig_scan( "\x55\x8B\xEC\x51\x8B\x81\x18\x02\x00\x00\x8B", "xxxxxxxxxxx" ) ) )
+        {
+            log_mgr.log_error( "offsets", "Failed to locate RBX::Humanoid::getMaxHealth() signature\n" );
+            return false;
+        }
+
+        log_mgr.log_debug( "offsets", "Located RBX::Humanoid::getMaxHealth() at 0x%x\n", get_max_health );
         return true;
     }
 
--- nord/src/data/roblox/offsets.hpp
+++ nord/src/data/roblox/offsets.hpp
@@ -7,17 +7,22 @@ namespace nord::rbx
     class offsets final
     {
        public:
-        offsets() : print( 0 ), get_task_scheduler( 0 )
+        offsets() : print( 0 ), get_task_scheduler( 0 ), get_health( 0 ), get_max_health( 0 )
         {
         }
 
         std::uintptr_t print;               // standard_out::get()->print()
         std::uintptr_t get_task_scheduler;  // task_scheduler::get()
+        std::uintptr_t get_health;          // humanoid::get_health()
+        std::uintptr_t get_max_health;      // humanoid::get_max_health()
 
         bool initialize();
 
        private:
         bool init_get_task_scheduler();
+        bool init_print();
+        bool init_get_health();
+        bool init_get_max_health();
     };
 
     // base class (constructs and saves address)
